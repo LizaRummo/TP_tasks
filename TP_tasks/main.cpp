d287a507a1439f8234d6bb6433179b0e68eb0510
Liza <rummo_liza@mail.ru>
2021-10-10 18:35:00 +0300

    modification

--- TP_tasks/main.cpp
+++ TP_tasks/main.cpp
@@ -15,32 +15,27 @@ vector<Purchase*> purchase;
 bool doubleSigned(const string&);
 bool stringSigned(const string&);
 bool intSigned(const string&);
-bool checking_number_products(int);
+bool check_prod_numb(int);
 int input_number();
-void base_products();
 
 void menu();
 void print_menu();
+void product_base();
+bool select_product();
 
-bool selection_products();
+void count_total();
 void output_purchase();
-void calculating_total_purchase();
 void output_check();
-
 void output_products();
 
-void main()
-{
+void main(){
 	setlocale(LC_ALL, "rus");
-	base_products();
-
+	product_base();
 	cout << "Добро пожаловать!" << endl;
-	while (1)
-		menu();
+	while (1) menu();
 }
 
-void base_products() {
-
+void product_base() {
 	products.push_back(Product("Курица", 250));
 	products.push_back(Product("Куриные яйца", 50));
 	products.push_back(Product("Молоко", 100));
@@ -55,12 +50,11 @@ void base_products() {
 void menu() {
 	print_menu();
 	int punkt_menu = input_number();
-	switch (punkt_menu)
-	{
+	switch (punkt_menu) {
 	case 1:
-		if (selection_products() == true) {
+		if (select_product() == true) {
 			output_purchase();
-			calculating_total_purchase();
+			count_total();
 			output_check();
 			system("cls");
 			cout << "Покупка была совершена успешно!" << endl;
@@ -80,9 +74,8 @@ void print_menu() {
 	cout << "  0. Завершить обслуживание" << endl;
 }
 
-bool selection_products() {
-	if (products.size() == 0)
-	{
+bool select_product() {
+	if (products.size() == 0) {
 		cout << "В магазине нет продуктов, приносим свои извинения." << endl;
 		return false;
 	}
@@ -97,16 +90,12 @@ bool selection_products() {
 			cout << "Выберите пункт " << endl;
 			int number_product = input_number();
 			if (number_product == 0) break;
-			else if (checking_number_products(number_product) == true)
-			{
+			else if (check_prod_numb(number_product) == true) 	{
 				Check* check = new Check;
 				check->input_purchase(products.at(number_product - 1));
 				purchase.push_back(check);
-			}
-			else
-				cout << "В магазине нет такого продукта. " << endl;
-		}
-		return true;
+			} else cout << "В магазине нет такого продукта. " << endl;
+		} return true;
 	}	
 }
 
@@ -118,7 +107,7 @@ void output_purchase() {
 	}
 }
 
-void calculating_total_purchase() {
+void count_total() {
 	double one_purchase, total_purchase = 0;
 	for (int i = 0; i < purchase.size(); i++) {
 		one_purchase = purchase[i]->getPurchasePrice();
@@ -140,7 +129,7 @@ void output_check() {
 	case 1:
 		fout << "         г. Санкт-Петербург" << endl;
 		fout << "      пр-т Маршала Жукова, д.60" << endl;
-		fout << "            КАССОВЫЙ ЧЕК" << endl << endl;
+		fout << "            Кассовый чек" << endl << endl;
 		for (int i = 0; i < purchase.size(); i++) {
 			fout << i + 1 << ". ";
 			purchase[i]->output_to_file(fout);
@@ -148,7 +137,7 @@ void output_check() {
 			total_purchase += one_purchase;
 		}
 		fout << "------------------------------------" << endl;
-		fout << "ИТОГО:";
+		fout << "Итого:";
 		fout.width(31);
 		fout << total_purchase << endl << endl;
 		fout.close();
@@ -170,49 +159,35 @@ void output_products() {
 	cout << endl;
 }
 
-bool checking_number_products(int number_products) {
-	if ((number_products <= products.size())&& (number_products>0))
-		return true;
-	else
-		return false;
+bool check_prod_numb(int number_products) {
+	if ((number_products <= products.size())&& (number_products>0)) return true;
+	else return false;
 }
 
 int input_number() {
 	string number_s;
-	while (1)
-	{
+	while (1) {
 		cout << ">> ";
 		cin >> number_s;
-		if (intSigned(number_s) == true)
-		{
+		if (intSigned(number_s) == true) {
 			int number = stoi(number_s);
 			return number;
-		}
-		else
-			cout << "Данный пункт отсутствует. Повторите ввод." << endl << ">> ";
+		} else 	cout << "Данный пункт отсутствует. Повторите ввод." << endl << ">> ";
 	}
 }
 
-bool intSigned(const string& s) // проверка на только цифры в строке
-{
-	if (s.find_first_not_of("0123456789", 0) == string::npos)
-		return true;
-	else
-		return false;
+//проверки на ввод
+bool intSigned(const string& s) {
+	if (s.find_first_not_of("0123456789", 0) == string::npos) return true;
+	else return false;
 }
 
-bool doubleSigned(const string& s) // проверка на только цифры в строке
-{
-	if (s.find_first_not_of("0123456789.", 0) == string::npos)
-		return true;
-	else
-		return false;
+bool doubleSigned(const string& s) {
+	if (s.find_first_not_of("0123456789.", 0) == string::npos) return true;
+	else return false;
 }
 
-bool stringSigned(const string& s) // проверка на буквы и пробел
-{
-	if (s.find_first_not_of(" qwertyuioplkjhgfdsamnbvcxzPOIUYTREWQLKJHGFDSAMNBVCXZ", 0) == string::npos)
-		return true;
-	else
-		return false;
+bool stringSigned(const string& s) {
+	if (s.find_first_not_of(" qwertyuioplkjhgfdsamnbvcxzPOIUYTREWQLKJHGFDSAMNBVCXZ", 0) == string::npos) return true;
+	else return false;
 }
